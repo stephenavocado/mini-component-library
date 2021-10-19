65a89d38f060d15cf737b3ecbef7b12cb7e3ffc1
stephenavocado <stephenwalter@chicagobooth.edu>
2021-10-19 23:55:24 +0000

    input first attempt

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -9,11 +9,56 @@ import VisuallyHidden from '../VisuallyHidden';
 const IconInput = ({
   label,
   icon,
-  width = 250,
+  width,
   size,
   placeholder,
 }) => {
-  return 'TODO';
-};
+  return (
+    <Wrapper 
+      style = {{ '--width': width + 'px'}}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper
 
+
+      />
+      <Input 
+        style = {{ '--width': width + 'px'}}
+        placeholder = "Search..."
+      />
+      <Underline />
+    </Wrapper>
+      );
+    };
+  
+  const Wrapper = styled.div`
+    width: var(--width);
+    border-radius: 2px;
+  `
+
+  const IconWrapper = styled.div`
+    width: 11px;
+    height: 16px;
+  `
+
+  const Input = styled.input`
+    border: none; 
+    width: var(--width);
+    height: 16px;
+    font-size: 16px;
+    padding-bottom: 4px;
+
+    &:focus {
+      border: none;
+    }
+  `
+
+  const Underline = styled.div`
+    height: 0px;
+    border: .5px solid ${COLORS.black};
+    background-color: ${COLORS.black};
+    border-radius: 2px;
+    width: auto;
+  `
+    
 export default IconInput;
